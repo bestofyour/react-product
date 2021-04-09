1f9e8e7edb0f06a86929350e0a9e46d244d1ece8
zourongzhi <zourongzhi1>
2021-04-09 11:19:54 +0800

    fix

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { Button } from 'antd';
+
+// import props from './props';
+
+function App () {
+  const Text="dsada"
+  const style = {
+    color: 'red'
+  }
+  return (<Button style={style}>{Text}</Button>)
 }
 
-export default App;
+export default App
